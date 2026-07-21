2814c73c0c56e2e56cbf8bd38f44d6226b60084e
v0l0v <vict0rl0pez@protonmail.com>
2026-07-21 22:36:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Jarre
+
+Proyecto espacial interactivo con visualización 3D y planetas procedimentales.
---
+++ planets.js
@@ -0,0 +1,8 @@
+const worldPalettes = [
+    { name: "NEON PRIME I", bg: 0x0a0515, c1: 0xff0055, c2: 0x00eeff, c3: 0x5500ff, type: 'neon' }, 
+    { name: "AURA II", bg: 0x110011, c1: 0xff00ff, c2: 0xaa00ff, c3: 0xff88ff, type: 'aura' },    
+    { name: "SOLARIS III", bg: 0x1a0a00, c1: 0xff8800, c2: 0xffd700, c3: 0xff0000, type: 'fire' },
+    { name: "TERRA NOVA IV", bg: 0x001100, c1: 0x00ff00, c2: 0x004400, c3: 0x88cc00, type: 'terra' },
+    { name: "TOX V", bg: 0x001105, c1: 0x00ff00, c2: 0x0088ff, c3: 0xccff00, type: 'toxic' },   
+    { name: "GLACIER VI", bg: 0x000511, c1: 0x00ffff, c2: 0xffffff, c3: 0x0055ff, type: 'ice' }     
+];
